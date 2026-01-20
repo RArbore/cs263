e3378478c83e0c5cfe019cf60e6720eef8113054
Max Willsey <me@mwillsey.com>
2026-01-20 09:45:34 -0800

    chap 1 2

--- lean/LoVE/LoVe01_TypesAndTerms_Demo.lean
+++ lean/LoVE/LoVe01_TypesAndTerms_Demo.lean
@@ -138,7 +138,6 @@ Application is left-associative: `f x y z` = `((f x) y) z`.
 
 #check ℕ
 #check ℤ
-#check Nat.add
 
 #check Empty
 #check Unit
@@ -213,59 +212,6 @@ opaque β : Type
 opaque γ : Type
 
 def someFunOfType : (α → β → γ) → ((β → α) → β) → α → γ :=
-  fun f g a ↦ f a (g (fun _b ↦ a))
-
-end LoVe
-
-
-/- # LoVe Exercise 1: Types and Terms
-
-Replace the placeholders (e.g., `:= sorry`) with your solutions. -/
-
-
-set_option autoImplicit false
-set_option tactic.hygienic false
-
-namespace LoVe
-
-
-/- ## Question 1: Terms
-
-Complete the following definitions, by replacing the `sorry` markers by terms
-of the expected type.
-
-Hint: A procedure for doing so systematically is described in Section 1.4 of
-the Hitchhiker's Guide. As explained there, you can use `_` as a placeholder
-while constructing a term. By hovering over `_`, you will see the current
-logical context. -/
-
-def I : α → α :=
-  fun a ↦ a
-
-def K : α → β → α :=
-  fun a b ↦ a
-
-def C : (α → β → γ) → β → α → γ :=
-  sorry
-
-def projFst : α → α → α :=
-  sorry
-
-/- Give a different answer than for `projFst`. -/
-
-def projSnd : α → α → α :=
-  sorry
-
-def someNonsense : (α → β → γ) → α → (α → γ) → β → γ :=
-  sorry
-
-
-/- ## Question 2: Typing Derivation
-
-Show the typing derivation for your definition of `C` above, on paper or using
-ASCII or Unicode art. Start with an empty context. You might find the
-characters `–` (to draw horizontal bars) and `⊢` useful. -/
-
--- write your solution in a comment here or on paper
+  fun f g a ↦ f a (g (fun b ↦ a))
 
 end LoVe
